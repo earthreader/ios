5ff109ad5c1a42bec4b153f0cb1ab6ab1e9fb45d
Jihyeok Seo <me@limeburst.net>
2014-04-27 18:50:00 +0900

    Initial Commit

---
+++ EarthReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EarthReader
+//
+//  Created by Jihyeok Seo on 4/27/14.
+//  Copyright (c) 2014 Earth Reader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ERAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ERAppDelegate class]));
+    }
+}
